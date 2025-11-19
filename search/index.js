e5717cd16a892409ce32295ce4ddb7466b56fc77
Andrii Tkachov <andriitkachov@Mac.local>
2025-11-19 18:49:04 +0100

    dataset

--- search/index.js
+++ search/index.js
@@ -1,12 +1,10 @@
 export const getSection = (num) => {
-  // 1. Находим span по data-number
   const span = document.querySelector(`span[data-number="${num}"]`);
 
-  // Защита: если span не найден
   if (!span) return null;
 
-  // 2. Находим ближайшего родителя с классом box
-  const parentBox = span.closest('.box');
+  const box = span.closest(".box");
 
-  return parentBox;
+  return box.dataset.section;
 };
+
